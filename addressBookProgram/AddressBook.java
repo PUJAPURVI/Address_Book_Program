87c7bd427164110afd5341d0aaaa6a2ab4d6b0ad
PUJAPURVI <nerkarpuja@gmail.com>
2023-01-07 15:53:22 +0530

    Added: Ability to add the multiple addressbook

--- addressBookProgram/AddressBook.java
+++ addressBookProgram/AddressBook.java
@@ -111,5 +111,38 @@ public class AddressBook {
             System.out.println("-------Contact Deleted Successfully-------");
         }
     }
+    public void contactOptions(AddressBook addressBook) {
+        //Show Menu for user to Select Operation on AddressBook
+        int choice;
+        do {
+            System.out.println("***** ADDRESS BOOK MANAGEMENT *****");
+            System.out.println("1. ADD NEW CONTACT\n2. EDIT CONTACT\n3. DELETE CONTACT" +
+                    "\n4. DISPLAY CONTACT\n5. EXIT");
+            System.out.println("Please Select the Operation Number : ");
+            choice = addressBook.sc.nextInt();
+
+            switch (choice) {
+                case 1:
+                    addressBook.addNewContact();    //Adding New Contact Details
+                    break;
+                case 2:
+                    addressBook.editContact();  //Edit Contact Details
+                    break;
+                case 3:
+                    addressBook.deleteContact();    //Delete the Contact Details
+                    break;
+                case 4:
+                    addressBook.displayContact();   //Show Contact Details
+                    break;
+                case 5:
+                    System.out.println("Thank You for Using Address Book.");
+                    break;
+                default:
+                    System.out.println("Please Select the Operation between 1 to 5 only.");
+                    break;
+            }
+        } while (choice != 5);
+    }
+
 }
 
