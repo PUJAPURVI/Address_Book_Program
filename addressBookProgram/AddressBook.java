a49956ec40a3c3ee869132bc23f7b1c1fe8dc181
PUJAPURVI <nerkarpuja@gmail.com>
2023-01-07 12:00:23 +0530

    Added: Welcome Message

---
+++ addressBookProgram/AddressBook.java
@@ -0,0 +1,8 @@
+package com.addressBookProgram;
+
+public class AddressBook {
+    public static void main(String[] args) {
+        System.out.println("~~~WEL-COME TO THE ADDRESS BOOK PROGRAM~~~");
+    }
+}
+
