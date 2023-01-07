b0dcc98b13bb6a5e3080febfa9d1adc803dc3233
PUJAPURVI <nerkarpuja@gmail.com>
2023-01-07 14:34:08 +0530

    Added: Ability to add a new contact in the addressbook

--- addressBookProgram/AddressBook.java
+++ addressBookProgram/AddressBook.java
@@ -1,8 +1,60 @@
 package com.addressBookProgram;
+import java.util.Scanner;
+/**
+ *
+ * @author Puja
+ *
+ */
+
 
 public class AddressBook {
-    public static void main(String[] args) {
-        System.out.println("~~~WEL-COME TO THE ADDRESS BOOK PROGRAM~~~");
+    /**
+     *
+     *Scanner - class is use for user input
+     *Contacts - class is use for to create a object of
+     *          contacts class for Getter and Setter method
+     */
+    Scanner sc = new Scanner(System.in);
+
+    //Created Contacts Object for Getter and Setter Methods
+    Contacts contacts = new Contacts();
+    /**
+     *
+     * This method is used to add the contact details
+     */
+    public void addNewContact() {
+        System.out.println("Enter the Contact Details -");
+        System.out.println("Enter the First Name :");
+        contacts.setFirstName(sc.next());
+        System.out.println("Enter the Last Name :");
+        contacts.setLastName(sc.next());
+        System.out.println("Enter the Address :");
+        contacts.setAddress(sc.next());
+        System.out.println("Enter the City :");
+        contacts.setCity(sc.next());
+        System.out.println("Enter the State :");
+        contacts.setState(sc.next());
+        System.out.println("Enter the Zip Code :");
+        contacts.setZip(sc.nextInt());
+        System.out.println("Enter the Phone Number :");
+        contacts.setPhoneNumber(sc.nextInt());
+        System.out.println("Enter the EMail ID :");
+        contacts.setEmail(sc.next());
+    }
+    /**
+     *
+     * This method is used to display the user input contact details
+     */
+    public void displayContact() {
+        System.out.println("Contact Details -");
+        System.out.println("First Name : "+contacts.getFirstName());
+        System.out.println("Last Name : "+contacts.getLastName());
+        System.out.println("Address : "+contacts.getAddress());
+        System.out.println("City : "+contacts.getCity());
+        System.out.println("State : "+contacts.getState());
+        System.out.println("Zip Code : "+contacts.getZip());
+        System.out.println("Phone Number : "+contacts.getPhoneNumber());
+        System.out.println("EMail ID : "+contacts.getEmail());
     }
 }
 
